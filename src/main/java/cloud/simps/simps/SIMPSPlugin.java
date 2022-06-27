6de126a4ef08ad9fb61fe2d821b7212ebcd2948c
LinusOP <32105232+LinusOP@users.noreply.github.com>
2022-06-27 22:06:51 +0200

    Initial commit

---
+++ src/main/java/cloud/simps/simps/SIMPSPlugin.java
@@ -0,0 +1,17 @@
+package cloud.simps.simps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SIMPSPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
